5b4f826ba8485fe985546046d2681d55d054de32
Elaija Heart Velasco <21-01226@g.batstate-u.edu.ph>
2022-12-02 15:59:20 +0800

    Add files via upload

--- Book Rental/src/Rental.java
+++ Book Rental/src/Rental.java
@@ -7,11 +7,12 @@
 
 import java.util.*;
 
+//imports packages that contain user-defined classes
 import models.*;
 import utils.*;
 
 public class Rental{
-//method for opening the system
+    //method to display menu
     public void app_menu () {
         println ("\n\n\t\t\t    ****** Book Rental System ******");
         while (true) {
@@ -21,13 +22,13 @@ public class Rental{
             println ("\t\t\t\t (2) Add book");
             println ("\t\t\t\t (3) Rent books");
             println ("\t\t\t\t (4) Display rented books");
-            println ("\t\t\t\t (5) Close");
+            println ("\t\t\t\t (5) Quit Application");
             println ("\n\t\t\t_________________________________________\n");
             input_choice();
         }
     } //end of method app_menu()
     
-//private method for inputting choice 
+    //method to get choice
     private void input_choice() {
         while(true) {
             String choice;
@@ -38,12 +39,10 @@ public class Rental{
                 if(i < 1 || i > 5) {
                     throw new ChoiceException();
                 }
-
-            //inputted choice will be passed to the paramenter 'i' of execute_choice() method
                 execute_choice(i); 
                 break;
             }
-        //handle input errors
+            //handle input errors
             catch(NumberFormatException e){
                 println("\n\t\t\tInvalid entry! Input must be a number.\n");
             }
@@ -53,7 +52,7 @@ public class Rental{
         }
     } //end of method input_choice()
 
-//private method for executing inputted choice 1,2,3,4, or 5
+    //method to execute choice 1,2,3,4, or 5
     private void execute_choice (int option) {
         switch (option) {
             case DISPLAY_BOOKS:
@@ -72,19 +71,29 @@ public class Rental{
                 view_rented_books();
             break;
 
-            case CLOSE_SHOP:
-            println("\n\n\t\t\t***Thank you for using the application!***\n\n");
-            System.exit(0);
+            case QUIT:
+                quit_app();
             break;
         }
     } //end of method execute_choice()
 
-//private method for displaying books
+    //constant variables
+    private static final int DISPLAY_BOOKS = 1;
+    private static final int ADD_BOOKS = 2;
+    private static final int RENT_BOOKS = 3;
+    private static final int VIEW_RENTED_BOOKS = 4;
+    private static final int QUIT = 5;
+
+    //creates arraylist named books 
+    ArrayList<Book> books = new ArrayList<>();
+
+    //method to display books
     private void display_books (String extra){
         if (books.isEmpty()) {
             println("\n\t\t\t No available books yet...");
             return;
         }
+        //sorts books arraylist 
         Collections.sort(books);
         println("\n\t\t\t\t\t***Book List***\n" + extra);
         for (int i = 0; i < books.size(); i++) {
@@ -92,7 +101,8 @@ public class Rental{
             books.get(i).print();
         }
     }
-//private method for adding books
+
+    //method to add books
     private void add_books() {
         switch(select_book()) {
 
@@ -117,10 +127,18 @@ public class Rental{
             break;
         }
         println("\n\t\t\tBooks successfully added!\n");
-    }
+    } // end of method add_books()
 
-//private method for selecting book to add
+    //constant variables
+    private static final int SCIENCE_FICTION = 1;
+    private static final int THRILLER = 2;
+    private static final int FANTASY = 3;
+    private static final int ROMANCE = 4;
+    private static final int SELF_HELP = 5;
+
+    //method to select book
     private int select_book() {
+        //select book menu
         println("\n\t\t\t\t\t***Add Book***\n");
         println("\t\t\t\t (1) Science Fiction");
         println("\t\t\t\t (2) Thriller");
@@ -131,7 +149,7 @@ public class Rental{
         while (true) {
             String input;
             try {
-            //input choice from select book menu
+                //input choice from select book menu
                 input = Input.string("\n\t\t\tEnter choice: ");
                 choice = Integer.parseInt(input);
                 if (choice < 1 || choice > 5) {
@@ -139,6 +157,7 @@ public class Rental{
                 }
                 break;
             }
+            //handle input errors
             catch(ChoiceException c) {
                 print ("\n\t\t\tInvalid entry! Enter a choice from 1 to 5.\n");
             }
@@ -146,26 +165,34 @@ public class Rental{
         return choice;
     } //end of method select_book()
 
-//Method for renting a book
+    //creates arraylist named rent
+    ArrayList<Rent> rent = new ArrayList<>();
+
+    //method to rent book
     private void rent_books() {
         if (books.isEmpty()) {
             println ("\n\t\t\t [!] No books available for rent.\n");
             return;
         }
-    //input book number
+
+        //prompt user to input book number
         display_books("\n\t\t\tSelect book you want to rent: \n");
         int num = Input.number ("\n\t\t\tEnter book number: ")-1;
         if (num >= books.size() || num < 0) {
             println("\n\t\t\tInvalid input!");
             return;
         }
-    //input book quantity
+
+        //prompt user to input book quantity
         Book bk = books.get(num);
         int quantity = Input.number("\n\t\t\tQuantity of book you want to rent: ");
+
+        //condition where user can't rent zero books
         if (quantity == 0) {
             println("\n\t\t\tYou're not able to rent 0 books!");
             return;
         }
+        //condition where user can't rent more than the total number of books
         if (quantity > bk.total) {
             println("\n\t\t\tYou're not able to rent " + quantity + " books! ");
             return;
@@ -178,7 +205,7 @@ public class Rental{
         }
     } //end of method rent_books()
 
-//method for displaying rented books
+    //method to display rented books
     public void view_rented_books() {
         if (rent.isEmpty()) {
             println("\n\t\t\tYou have not rented any books yet...");
@@ -191,7 +218,9 @@ public class Rental{
         }
     } //end of method view_rented_books()
 
-    public void close_shop() {
+    //method to quit application
+    public void quit_app() {
+        println("\n\n\t\t\t***Thank you for using the application!***\n\n");
         System.exit(0);
     }
     private void print(String message) {
@@ -200,19 +229,4 @@ public class Rental{
     private void println(String message){
         System.out.println(message);
     }
-
-    private static final int DISPLAY_BOOKS = 1;
-    private static final int ADD_BOOKS = 2;
-    private static final int RENT_BOOKS = 3;
-    private static final int VIEW_RENTED_BOOKS = 4;
-    private static final int CLOSE_SHOP = 5;
-
-    private static final int SCIENCE_FICTION = 1;
-    private static final int THRILLER = 2;
-    private static final int FANTASY = 3;
-    private static final int ROMANCE = 4;
-    private static final int SELF_HELP = 5;
-
-    ArrayList<Rent> rent = new ArrayList<>();
-    ArrayList<Book> books = new ArrayList<>();
-}
+} //end 
